0548b5b942b6a5c83edc4a5ad7502743ecedb7f6
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-05-17 16:59:08 -0700

    top navbar working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        typography: ["IBM Plex Sans"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
