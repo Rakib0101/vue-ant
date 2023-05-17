ae422a17f614134cfd4d23acf72fb696543dc92f
Rakib0101 <rakib.01jan@gmail.com>
2023-05-17 08:34:39 +0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <a-dropdown>
+    <a class="ant-dropdown-link" @click.prevent>
+      Hover me
+      <DownOutlined />
+    </a>
+    <template #overlay>
+      <a-menu>
+        <a-menu-item>
+          <a href="javascript:;">1st menu item</a>
+        </a-menu-item>
+        <a-menu-item>
+          <a href="javascript:;">2nd menu item</a>
+        </a-menu-item>
+        <a-menu-item>
+          <a href="javascript:;">3rd menu item</a>
+        </a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
+</template>
+<script>
+  import { DownOutlined } from '@ant-design/icons-vue';
+  export default {
+    components: {
+      DownOutlined,
+    },
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import './assets/tailwind.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Antd);
+
+app.mount('#app')
